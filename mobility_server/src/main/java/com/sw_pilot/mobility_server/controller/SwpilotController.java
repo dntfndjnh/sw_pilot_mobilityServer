8122e6d5601e67e2a3ece64a642dfd1e6d03b154
Robbie <Robbie@email.com>
2025-06-27 16:21:25 +0900

    테스트컨트롤러추가

--- mobility_server/src/main/java/com/sw_pilot/mobility_server/controller/SwpilotController.java
+++ mobility_server/src/main/java/com/sw_pilot/mobility_server/controller/SwpilotController.java
@@ -1,7 +1,10 @@
 package com.sw_pilot.mobility_server.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class SwpilotController {
@@ -9,6 +12,15 @@ public class SwpilotController {
    @GetMapping("/report")
     public String Show_report(){
         return "report";
+    }
+
+    @RestController // 중요: @Controller + @ResponseBody 기능
+    public class EchoController {
+
+        @GetMapping("/test")
+        public String echo(@RequestParam(name = "msg", required = false, defaultValue = "메시지가 없습니다.") String msg) {
+            return msg; // 입력받은 문자열을 그대로 반환
+        }
     }
 
 }
